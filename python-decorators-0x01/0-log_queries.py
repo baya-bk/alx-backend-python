01849ab8da97a6bb0ac599ec4af2f9354b500593
Behailu <behailubedada1994@gmail.com>
2025-07-18 13:51:31 +0300

    feat: implement cache_query decorator to cache database query results

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,13 +1,13 @@
 import sqlite3
 import functools
+from datetime import datetime
 
 # Decorator to log SQL queries
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        query = kwargs.get('query') if 'query' in kwargs else (args[0] if args else None)
-        if query:
-            print(f"Executing SQL Query: {query}")
+        query = kwargs.get("query") if "query" in kwargs else args[0]
+        print(f"[{datetime.now()}] Executing SQL Query: {query}")
         return func(*args, **kwargs)
     return wrapper
 
@@ -22,7 +22,7 @@ def fetch_all_users(query):
     return results
 
 
-# Fetch users while logging the query
+# Example usage
 if __name__ == "__main__":
     users = fetch_all_users(query="SELECT * FROM users")
     print(users)
